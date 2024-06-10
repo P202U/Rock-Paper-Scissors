260615e9a421152610202d3c4cbfb7f63f0c5d6b
Piyush <156369604+P202U@users.noreply.github.com>
2024-06-11 00:13:54 +0530

    Write single round logic
    Improve the overall logic above to match the round logic

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3);
-
-    if (randomNumber === 0) {
-        return `ROCK!`;
-    } else if (randomNumber === 1) {
-        return `PAPER!`;
-    } return `Scissors`;
+    const computerChoice = Math.floor(Math.random() * 3);
+    return computerChoice;
 }
 
 function getHumanChoice() {
     const humanChoice = prompt(`Play your choice`);
 
-    return humanChoice.toLowerCase();
+    humanChoice.toLowerCase(); // Changing to lower case
+
+    if (humanChoice === `rock`) {
+        return 0;
+    } else if (humanChoice === `paper`) {
+        return 1;
+    } return 2;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (((humanChoice - computerChoice) % 3 + 3) % 3 === 1) {
+        console.log(`You won!`);
+        humanScore++;
+    } else if (((humanChoice - computerChoice) % 3 + 3) % 3 === 2) {
+        console.log(`You Lost!`);
+        computerScore++;
+    } else {
+        console.log(`It's a tie!`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
