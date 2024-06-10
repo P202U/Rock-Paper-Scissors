1d0f7ff9303f2b4bdeafb5d9337507215597e4c5
Piyush <156369604+P202U@users.noreply.github.com>
2024-06-10 22:55:01 +0530

    Get computer choice

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return `ROCK!`;
+    } else if (randomNumber === 1) {
+        return `PAPER!`;
+    } return `Scissors`;
+}
+
